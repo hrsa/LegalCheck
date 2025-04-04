3628ebfc38b295cc4e806f5ea963202ac1967d96
Anton Cherednichenko <anton@anton.eco>
2025-04-04 15:24:06 +0200

    Switch authentication transport to cookies
    Replaced Bearer transport with Cookie transport for authentication. Updated cookie settings to align with application token expiration configurations. Adjusted `docker-compose.yml` to include networks for clearer service isolation.

--- app/core/auth.py
+++ app/core/auth.py
@@ -1,14 +1,16 @@
-from fastapi_users.authentication import BearerTransport, JWTStrategy, AuthenticationBackend
+from fastapi_users.authentication import BearerTransport, JWTStrategy, AuthenticationBackend, CookieTransport
 
 from app.core.config import settings
 
 bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
+
+cookie_transport = CookieTransport(cookie_name="legalcheck_access_token", cookie_max_age=settings.ACCESS_TOKEN_EXPIRE_SECONDS, cookie_secure=False, cookie_httponly=True, cookie_samesite="lax")
 
 def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(secret=settings.SECRET_KEY, lifetime_seconds=settings.ACCESS_TOKEN_EXPIRE_SECONDS)
 
 auth_backend = AuthenticationBackend(
     name="jwt",
-    transport=bearer_transport,
+    transport=cookie_transport,
     get_strategy=get_jwt_strategy,
 )
